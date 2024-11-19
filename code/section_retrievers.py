e12c0942c41967ee59753e9cdddc5c96ee40f38e
Neal <neal301@gmail.com>
2024-11-19 17:13:26 -0500

    modified edgar retriever

--- code/section_retrievers.py
+++ code/section_retrievers.py
@@ -2,6 +2,7 @@ from edgar import *
 import time
 from edgar import set_identity
 import re
+import pandas as pd
 
 
 class EdgarRetriever:
@@ -28,30 +29,69 @@ class EdgarRetriever:
         }
 
     @staticmethod
+    def _clean_text(selected_raw_chunk_text: List[str]) -> list:
+        for i in range(len(selected_raw_chunk_text)):
+            selected_raw_chunk_text[i] = selected_raw_chunk_text[i].lower()
+            selected_raw_chunk_text[i] = selected_raw_chunk_text[i].replace("\n"," ")
+            selected_raw_chunk_text[i] = selected_raw_chunk_text[i].replace("•", " ")
+            selected_raw_chunk_text[i] = selected_raw_chunk_text[i].replace("table of contents","")
+            selected_raw_chunk_text[i] = " ".join(selected_raw_chunk_text[i].split())
+        return selected_raw_chunk_text
+
+    @staticmethod
     def _is_valid(credentials: str) -> bool:
         validpattern = r"[A-Za-z]+\s[A-Za-z]+\s[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}"
         return bool(re.match(validpattern, credentials))
 
     def get_filings(self):
-        filings = {}
+        companies = []
+        years = []
+        sections = []
+        text = []
 
         for company in self.companies:
+            chunked_tenks = {}
+
             try:
                 time.sleep(self.sleep_time)
 
                 tenks = Company(company).get_filings(form="10-K").latest(self.years_back)
 
                 if self.years_back == 1:  # deal with single year case, edgartools returns a single object
-                    filings[company] = {tenks.filing_date: tenks.sections()}
+                    chunked_tenks[tenks.filing_date] = tenks.sections()
                 else:
-                    filings[company] = {tenks[i].filing_date: tenks[i].sections() for i in range(self.years_back)}
+                    chunked_tenks = {tenks[i].filing_date: tenks[i].sections() for i in range((len(tenks)))}
                 
-                for section in self.section_patterns:
+                for year in chunked_tenks:
+                    raw_chunked_text = chunked_tenks[year]
+
+                    for desired_section in self.section_patterns:
+                        start_pattern = self.section_patterns[desired_section]['start']
+                        end_pattern = self.section_patterns[desired_section]['end']
+
+                        index_start = None
+                        index_end = None
+
+                        for i in range(len(raw_chunked_text)):
+                            if re.search(start_pattern, raw_chunked_text[i].lower()):
+                                index_start = i
+                            elif re.search(end_pattern, raw_chunked_text[i].lower()):
+                                index_end = i
+                                break
+
+                        if index_start is not None and index_end is not None:
+                            selected_raw_chunk_text = raw_chunked_text[index_start:index_end]
+                            cleaned_chunk_text = self._clean_text(selected_raw_chunk_text)
+                        else:
+                            print("Could not find section: ", desired_section, " for company: ", company, " in year: ", year)
                         
+
+
 
             except Exception as e:
                 print("Error retrieving filings for company: ", company)
-        return filings
+        
+        return None
 
 
 if __name__ == '__main__':
